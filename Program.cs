fc9b6dd02a9b41fd5d691fb5d7ff215242433b9c
Anton <krroll@bk.ru>
2023-01-18 00:22:46 +0800

    finish

--- Program.cs
+++ Program.cs
@@ -9,18 +9,40 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+// Размерность массива
 int nElement = 4;
 
 string [] array = GetArray();
-Console.WriteLine(string.Join(", ",array));
+if (FindElement(array)[0] == String.Empty)
+Console.WriteLine("[]");
+else
+Console.WriteLine(string.Join(", ",FindElement(array)));
 
 string [] GetArray()
 {
     string [] result = new string [nElement];
     for (int i = 0; i < result.Length; i++)
     {
-        Console.WriteLine("Введите "+i+" элемент массива");
+        Console.WriteLine("Введите "+(i+1)+" элемент массива");
         result[i]=Console.ReadLine();
     }
     return result;
+}
+
+string [] FindElement (string [] array)
+{
+    string [] threeSymbol = {""};
+    int numArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            threeSymbol[numArray]=array[i];
+            numArray++;
+            if (i+1 < array.Length)
+            Array.Resize(ref threeSymbol, threeSymbol.Length + 1);
+        }    
+    }
+    return threeSymbol;
 }
